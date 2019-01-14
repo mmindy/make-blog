5dc1cf7041914b84e00119d229f25d3fc576bc6b
minji <flowing_mj@naver.com>
2019-01-14 18:58:56 +0900

    0114 set root component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SassComponent from "./SassComponent";
+import Root from "./Root";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<SassComponent />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
